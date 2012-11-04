aca76d001a842038f6351c5f36ddb1ac1550864b
rtaylor <dick@realtimegraphics.co.uk>
2012-11-04 11:17:11 +0000

    test

--- pi/test/main.c
+++ pi/test/main.c
@@ -1,25 +1,24 @@
-/**
-
- $Id$
- $Date$
- $File$
- $Author$
- $Revision$
- $Source$
-
- * @file        hello.c
- * @author      @$Author$
- * @date	@$Date$
- * @version	@$Revision$
- *
- * $Id$
-
-**/
-
-
-int main()
-{
-
-	printf("\nHello world\n");
-	return(1);
-}
+.git_filters/rcs-keywords.clean/**
+.git_filters/rcs-keywords.clean
+.git_filters/rcs-keywords.clean $Id$
+.git_filters/rcs-keywords.clean $Date$
+.git_filters/rcs-keywords.clean $File$
+.git_filters/rcs-keywords.clean $Author$
+.git_filters/rcs-keywords.clean $Revision$
+.git_filters/rcs-keywords.clean $Source$
+.git_filters/rcs-keywords.clean
+.git_filters/rcs-keywords.clean * @file        hello.c
+.git_filters/rcs-keywords.clean * @author      @$Author$
+.git_filters/rcs-keywords.clean * @date	@$Date$
+.git_filters/rcs-keywords.clean * @version	@$Revision$
+.git_filters/rcs-keywords.clean *
+.git_filters/rcs-keywords.clean * $Id$
+.git_filters/rcs-keywords.clean
+.git_filters/rcs-keywords.clean**/
+.git_filters/rcs-keywords.clean
+.git_filters/rcs-keywords.cleanint main()
+.git_filters/rcs-keywords.clean{
+.git_filters/rcs-keywords.clean
+.git_filters/rcs-keywords.clean	printf("\nHello world\n");
+.git_filters/rcs-keywords.clean	return(1);
+.git_filters/rcs-keywords.clean}
