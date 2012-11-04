90d70938125abc86fce62e35fbd1cd8e1c4c2e79
rtaylor <dick@realtimegraphics.co.uk>
2012-11-04 12:10:24 +0000

    Message

--- pi/test/main.c
+++ pi/test/main.c
@@ -1,31 +1,14 @@
-<<<<<<< HEAD
 /*******************************************************
 force a merge
-=======
-/**
 
->>>>>>> 0ce86686579595406fc4bf7cf22e41050a8467f0
  $Id$
  $Date$
  $File$
  $Author$
  $Revision$
  $Source$
-<<<<<<< HEAD
 test
 *******************************************************/
-/**
-=======
-
->>>>>>> 0ce86686579595406fc4bf7cf22e41050a8467f0
- * @file        hello.c
- * @author      @$Author$
- * @date	@$Date$
- * @version	@$Revision$
- *
- * $Id$
-
-**/
 
 int main()
 {
